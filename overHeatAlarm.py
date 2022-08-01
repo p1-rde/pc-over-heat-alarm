16b5c39c6c258b6a0dd5bbe7fcf5744c536d8f34
Rahul Dhole <102797663+p1-rde@users.noreply.github.com>
2022-08-01 16:28:35 +0200

    Add files via upload

---
+++ overHeatAlarm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import subprocess as sp
+import notify2
+import time
+
+while(True):
+    cpuTemp = sp.run("sensors | grep -w 'CPU'|awk '{print $2}'", shell=True, capture_output=True)
+    cpuTemp = cpuTemp.stdout[0]
+
+    if cpuTemp>40:
+        notify2.init('Alarm')
+        n = notify2.Notification("Overheat Alarm!",
+                                "CPU is "+str(cpuTemp)+"\N{DEGREE SIGN}C",
+                                "notification-message-im"
+                                )
+        n.show()
+    time.sleep(300)
